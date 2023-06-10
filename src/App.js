167ada23672786174b848bbdcb966f67a2140607
Your Name <you@example.com>
2023-06-10 19:44:19 +0200

    my first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem />
+      <p>testing react</p>
+    </div>
+  );
+}
+
+export default App;
