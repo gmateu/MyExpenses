9ddd5a6b7346fedcf98fa7e959d948652fe436d6
Your Name <you@example.com>
2023-06-11 08:15:37 +0200

    added card as a wrapper

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,19 @@
 //https://vscode.dev/tunnel/manjarodesktop/home/guillem/react/01-starting-setup
-import "./ExpenseItem.css";
-import "./ExpenseDate";
+import Card from "./Card"
 import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 function ExpenseItem(props) {
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">{expenseAmount}€</div>
       </div>
-    </div>
+    </Card>
   );
 }
 export default ExpenseItem;
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,3 +1,4 @@
+import Card  from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 
@@ -5,7 +6,7 @@ import './Expenses.css';
 function Expenses(props) {
   const expenses = props.expenses;
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
@@ -21,7 +22,7 @@ function Expenses(props) {
         amount={expenses[2].amount}
         date={expenses[2].date}
       />
-    </div>
+    </Card>
   );
 }
 
