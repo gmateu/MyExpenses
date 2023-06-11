b359629d2855574b1b1903d2a4a3f14a9396a2a1
Your Name <you@example.com>
2023-06-11 07:20:35 +0200

    passant paràmetres a

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,19 @@
-function ExpenseItem(){
-    return(
-        <h2>Expense Item</h2>
-    );
-}
+//https://vscode.dev/tunnel/manjarodesktop/home/guillem/react/01-starting-setup
+import './ExpenseItem.css'
+function ExpenseItem(props) {
 
+    const expenseDate = props.date
+    const expenseTitle = props.title
+    const expenseAmount = props.amount
+
+  return (
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>{expenseAmount}€</div>
+      </div>
+    </div>
+  );
+}
 export default ExpenseItem;
