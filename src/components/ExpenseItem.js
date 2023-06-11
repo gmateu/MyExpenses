64016a04371c6b1f25ceeb3ff2597e8c0bdd8bda
Your Name <you@example.com>
2023-06-11 07:26:23 +0200

    format de data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,27 @@
 import './ExpenseItem.css'
 function ExpenseItem(props) {
 
-    const expenseDate = props.date
     const expenseTitle = props.title
     const expenseAmount = props.amount
 
+    const month=props.date.toLocaleString('ca-ES',{month: 'long'})
+    const day=props.date.toLocaleString('ca-ES',{day: '2-digit'})
+    const year=props.date.getFullYear()
+
+
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>
+            {month}
+        </div>
+        <div>
+            {year}
+        </div>
+        <div>
+            {day}
+        </div>
+      </div>
       <div className='expense-item__description'>
         <h2>{expenseTitle}</h2>
         <div className='expense-item__price'>{expenseAmount}€</div>
